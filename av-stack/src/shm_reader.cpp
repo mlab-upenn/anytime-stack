1a69ec23bff0524177aa96803a062603b05949a1
sidd-1234 <78105707+sidd-1234@users.noreply.github.com>
2022-08-10 17:26:37 -0400

    ROS-2 implementation of shm_reader

--- av-stack/src/shm_reader.cpp
+++ av-stack/src/shm_reader.cpp
@@ -1,25 +1,56 @@
+#include <chrono>
+#include <memory>
+#include <vector>
+#include <cmath>
 #include <iostream>
+#include <fstream>
+#include <Eigen/Core>
+#include <unsupported/Eigen/NonLinearOptimization>
+#include <unsupported/Eigen/NumericalDiff>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int32.hpp"
+
 using namespace std;
+using namespace Eigen;
 
-int main()
+# define PI 3.14159265358979323846
+
+class SHMRead : public rclcpp::Node
 {
-    // ftok to generate unique key
-    key_t key = ftok("shmfile", 65);
-    
-    // shmget returns an identifier in shmid
-    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
-  
-    // shmat to attach to shared memory
-    int *data = (int*) shmat(shmid, (void*)0, 0);
+	public:
 
-    cout << "Read data: " << *data << endl;
+	key_t key = ftok("shmfile", 65);
+	int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+	
+	SHMRead() : Node("SHMRead")
+	{
+        publisher_ = this->create_publisher<std_msgs::msg::Int32>("/read_data", 1);
+        timer_ = this->create_wall_timer(100ms, [this]{ timer_callback(); });
+	}
 
-    shmdt(data);
+	private:
+	
+	void timer_callback()
+	{
+	 auto drive_msg = std_msgs::msg::Int32();
+	 int *data = (int*) shmat(shmid, (void*)0, 0);
+	 drive_msg.data = *data;
+	 publisher_->publish(drive_msg);
+	}
 
-    shmctl(shmid, IPC_RMID, NULL);
+	rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_;
+	
+	rclcpp::TimerBase::SharedPtr timer_;
+};
 
-    return 0;
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SHMRead>());
+  rclcpp::shutdown();
+  return 0;
 }
